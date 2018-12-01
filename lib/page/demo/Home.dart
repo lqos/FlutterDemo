78eb2ec2402e7bf4c2470605a2b517a50e3ca05d
Liuliqiang <mylike_45@163.com>
2018-12-01 17:49:15 +0800

    返回键和左上角返回监听

--- lib/page/demo/Home.dart
+++ lib/page/demo/Home.dart
@@ -48,33 +48,42 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: _bodys[_tabIndex],
-      bottomNavigationBar: new BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          new BottomNavigationBarItem(
-              icon: getIcon(0),
-              activeIcon: getActiveIcon(0),
-              title: new Text("首页")),
-          new BottomNavigationBarItem(
-              icon: getIcon(1),
-              activeIcon: getActiveIcon(1),
-              title: new Text("发现")),
-          new BottomNavigationBarItem(
-              icon: getIcon(2),
-              activeIcon: getActiveIcon(2),
-              title: new Text("我的")),
-        ],
-        //设置显示的模式
-        type: BottomNavigationBarType.fixed,
-        //设置当前的索引
-        currentIndex: _tabIndex,
-        //tabBottom的点击监听
-        onTap: (index) {
-          setState(() {
-            _tabIndex = index;
-          });
-        },
+    return new WillPopScope(
+      //监听返回键或者左上角返回
+      onWillPop: () {
+        print("点击返回");
+        if (Navigator.canPop(context)) {
+          print("退出app");
+        }
+      },
+      child: new Scaffold(
+        body: _bodys[_tabIndex],
+        bottomNavigationBar: new BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            new BottomNavigationBarItem(
+                icon: getIcon(0),
+                activeIcon: getActiveIcon(0),
+                title: new Text("首页")),
+            new BottomNavigationBarItem(
+                icon: getIcon(1),
+                activeIcon: getActiveIcon(1),
+                title: new Text("发现")),
+            new BottomNavigationBarItem(
+                icon: getIcon(2),
+                activeIcon: getActiveIcon(2),
+                title: new Text("我的")),
+          ],
+          //设置显示的模式
+          type: BottomNavigationBarType.fixed,
+          //设置当前的索引
+          currentIndex: _tabIndex,
+          //tabBottom的点击监听
+          onTap: (index) {
+            setState(() {
+              _tabIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
