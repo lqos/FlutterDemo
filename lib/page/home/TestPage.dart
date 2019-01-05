7993a9cd37153421eec314a82d7e18480ed6ef96
Liuliqiang <mylike_45@163.com>
2019-01-05 21:48:21 +0800

    去除json_serializable

---
+++ lib/page/home/TestPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+///测试验证界面
+class TestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('TestPage'),
+      ),
+    );
+  }
+}
